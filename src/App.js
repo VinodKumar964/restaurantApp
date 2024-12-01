291c40680075a2cc903d43b3591af68fec7dbde7
VinodKumar964 <vinodnani95024@gmail.com>
2024-12-01 04:41:25 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './components/Home';
+import './App.css'; // Assuming you have some global styles here
+
+const App = () => {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+};
+
+export default App;
